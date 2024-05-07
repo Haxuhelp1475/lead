5ef582d7c639593ea6b4c1ea9576cba03bbcca25
thanhthao01 <165782463+thanhthao01@users.noreply.github.com>
2024-05-07 16:24:40 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPORTS NEWS</title>
+    <link rel="stylesheet" href="Trangchu.css">
+</head>
+
+<body>
+    <footer>
+        <div class="foot">
+            <ul>
+                <li class="foot foot-infor">
+                    <h3><u>BÁO ĐIỆN TỬ SPORTS NEWS</u></h3>
+                    <p><label id="label-color" for="#">Cơ sở pháp lý:</label>Giấy phép số 321/GP-BTTTT ngày 15/06/2024
+                        do BTT&TT cấp.</p>
+                    <p><label id="label-color" for="#">Cơ sở độc quyền:</label>Thể thao & Văn hóa giữ bản quyền nội dung
+                        trên website này.</p>
+                </li>
+                <li class="foot foot-contact">
+                    <h3><u>THÔNG LIÊN HỆ</u></h3>
+                    <P>Hotline:0989.826.743</P>
+                    <p>Email:sportsnews@gmail.com</p>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPORTS NEWS</title>
+    <link rel="stylesheet" href="Trangchu.css">
+</head>
+
+<body>
+    <div id=" logo">
+        <h1><img src="https://i.imgur.com/Cc3S8Di.png"></h1>
+    </div>
+    <header>
+    </header>
+</body>
+
+</html>
